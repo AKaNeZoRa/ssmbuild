11174551cfa00028a985d04af42809cbcc983240
AKANEZORA <354367686@qq.com>
2021-10-28 14:29:20 +0800

    Initial commit

---
+++ src/main/java/com/xxx/service/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package com.xxx.service;
+
+import com.xxx.dao.BookMapper;
+import com.xxx.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService {
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
